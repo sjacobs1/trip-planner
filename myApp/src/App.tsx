811bdd022924bc8cef575021d2b61a362e1f2908
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-07-11 09:31:25 +0200

    chore: :tada:

---
+++ myApp/src/App.tsx
@@ -0,0 +1,9 @@
+// import './App.css'
+
+function App() {
+  return (
+    <p>can you see this text?</p>
+  );
+}
+
+export default App;
---
+++ myApp/src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './app.css'
+import App from './App.js'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)

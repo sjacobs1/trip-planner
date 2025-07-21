e382db17f20954ff631b03ac34d5f07c250c2855
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-07-21 10:40:23 +0200

    refactor: :recycle:

--- myApp/src/services/getUnsplashImage.ts
+++ myApp/src/services/getUnsplashImage.ts
@@ -1,7 +1,7 @@
 import { UnsplashImage } from "../models/unsplashImage";
 
 export async function getUnsplashImage(): Promise<UnsplashImage> {
-    const response = await fetch("https://api.unsplash.com/photos/random?collections=11484844&orientation=landscape&content_filter=high&count=1&client_id=4kAlW_Ksh_SsGsHYPvB8C2lSuzwfGw87p23PmyIXghk")
+    const response = await fetch("https://api.unsplash.com/photos/random?collections=11484844&orientation=landscape&content_filter=high&count=1")
     const data = await response.json();
     return Array.isArray(data) ? data[0] : data;
 }
